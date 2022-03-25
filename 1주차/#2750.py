ea2829ebb1edc64037af488e52656667520f6839
ChaeyeonHan <kate2236e@naver.com>
2022-03-25 09:15:58 +0900

    #2750 추가

--- 1주차/#2750.py
+++ 1주차/#2750.py
@@ -5,7 +5,7 @@ for _ in range(n):
     nums.append(int(input()))
 
 nums.sort()  # 내장함수
-nums = nums.sorted()  # 정렬된 리스트 반환
+#nums = nums.sorted()  # 정렬된 리스트 반환
 for i in range(n):
     print(nums[i])
 
@@ -23,5 +23,21 @@ for i in range(n):
             min_index = j
     nums[min_index], nums[i] = nums[i], nums[min_index]  # 위치 바꾸기(최소값을 앞으로 보내준다)
 
+for i in nums:
+    print(i)
+
+#3 버블 정렬
+def bubble_sort(a):
+    n = len(a)
+    for i in range(n):
+        for j in range(n-i-1):
+            if a[j] > a[j+1]:
+                a[j], a[j+1] = a[j+1], a[j]
+
+n = int(input())
+nums=[]
+for i in range(n):
+    nums.append(int(input()))
+bubble_sort(nums)
 for i in nums:
     print(i)
